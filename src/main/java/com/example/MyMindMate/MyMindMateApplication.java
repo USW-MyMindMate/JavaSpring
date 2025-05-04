0eb008524e5a302beef59410fc5e386365702cb2
JJeong11 <ask796629@naver.com>
2025-05-05 01:09:42 +0900

    Initial commit

---
+++ src/main/java/com/example/MyMindMate/MyMindMateApplication.java
@@ -0,0 +1,13 @@
+package com.example.MyMindMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMindMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMindMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MyMindMate/MyMindMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyMindMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMindMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
