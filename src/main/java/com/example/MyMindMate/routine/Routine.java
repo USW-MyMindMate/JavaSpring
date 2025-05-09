39b8be6911a2149c799ab7c70f20bcd12204ee29
yerin028 <syr0002@gmail.com>
2025-05-09 12:43:16 +0900

    fix: routine 및 routinelog 엔티티에 어노테이션 및 컬럼 제약 조건 추가 & distribution 수정

--- src/main/java/com/example/MyMindMate/routine/Routine.java
+++ src/main/java/com/example/MyMindMate/routine/Routine.java
@@ -1,26 +1,37 @@
 package com.example.MyMindMate.routine;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalTime;
 
 @Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "routines")
 public class Routine {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
 
+    @Column(nullable = false)
     private Long userId;
 
+    @Column(name = "created_by", nullable = false)
     private Long createBy;
 
+    @Column(nullable = false)
     private String title;
 
+    @Column(nullable = false)
     private LocalTime time;
 
+    @Column(name = "day_of_week", nullable = false)
     private String dayOfWeek;
 }
--- src/main/java/com/example/MyMindMate/routine/RoutineLog.java
+++ src/main/java/com/example/MyMindMate/routine/RoutineLog.java
@@ -1,21 +1,30 @@
 package com.example.MyMindMate.routine;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "routine_logs")
 public class RoutineLog {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
 
+    @Column(nullable = false)
     private Long routineId;
 
+    @Column(nullable = false)
     private Long userId;
 
+    @Column(nullable = false)
     private boolean isCompleted;
 }
