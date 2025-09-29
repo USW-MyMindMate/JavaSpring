6375b26a3440ef3379f99b7f8033ebb3969c9f88
yerin028 <syr0002@gmail.com>
2025-09-29 14:10:32 +0900

    feat: emotion recording 기능

--- src/main/java/com/example/MyMindMate/mood/dto/MoodRecordRequest.java
+++ src/main/java/com/example/MyMindMate/mood/dto/MoodRecordRequest.java
@@ -1,7 +1,18 @@
 package com.example.MyMindMate.mood.dto;
 
+import com.example.MyMindMate.mood.MoodTypeName;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class MoodRecordRequest {
-    private Long moodTypeId;
+    //private Long moodTypeId;
+    private MoodTypeName moodTypeName;
     private String reason;
     private Long userId;
 }
